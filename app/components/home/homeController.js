654ac9de55aab08090fe9e449091880e0e5938a5
Almir Junior <oliveira_junior90@hotmail.com>
2018-06-27 00:20:21 -0300

    home carousel

--- app/components/home/homeController.js
+++ app/components/home/homeController.js
@@ -1,12 +1,37 @@
 const homeController = function(artistService){
     
     var vm = this
-    vm.objects = []
+    vm.topArtists = []
+    vm.topTracks = []
 
     const topArtists = artistService.getTopArtists()
     topArtists.then(result => {
-        console.log(result)
-        vm.objects = result.data.artists.artist
+
+        vm.topArtists = result.data.artists.artist
+            .map(result => {
+                return {
+                    name: result.name,
+                    image: result.image[3]['#text'],
+                    listeners: result.listeners
+                }
+            })
+    })
+
+    const topTracks = artistService.getTopTracks()
+    topTracks.then(result=> {
+        
+        vm.topTracks = result.data.tracks.track
+            .map(result => {
+                return {
+                    name : result.name,
+                    url : result.url,
+                    artist : result.artist.name,
+                    image : result.image[1]['#text']
+                }
+            })
+
+        console.log(vm.topTracks)
+
     })
 
 }
--- app/providers/artist.service.js
+++ app/providers/artist.service.js
@@ -7,6 +7,11 @@ const artistService = function($http, config) {
             .get(config.apiUrl+"?method=chart.gettopartists&page=1&limit=5&format=json&api_key="+config.apiKey)
     }
 
+    vm.getTopTracks = function() {
+        return $http
+            .get(config.apiUrl+"?method=tag.gettoptracks&page=1&limit=9&tag=disco&format=json&api_key="+config.apiKey)
+    }
+
     
 }
 
