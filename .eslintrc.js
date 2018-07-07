f4089517149b0fc674d3fadedc6973d0aa5d9da1
Marco Carvalho <marcolucio27@gmail.com>
2018-07-07 03:31:21 -0300

    add eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "extends": [
+    "eslint:recommended"
+  ],
+  "parserOptions": {
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "env": {
+    "browser": true,
+    "node": true
+  }
+}
