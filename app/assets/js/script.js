3692461a6292177051af6be2b146f694aee138cf
Saul Vieira <vieira_saul@yahoo.com.br>
2019-08-04 04:29:23 -0300

    index
    layout da home finalizado (desktop)

---
+++ app/assets/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+	var swiper = new Swiper('.swiper-container', {
+		spaceBetween: 40,
+		slidesPerView: 3,
+		slidesPerGroup: 3,
+  		pagination: {
+    		el: '.swiper-pagination',
+    		clickable: true,
+      	},
+    });
+});
+
+
