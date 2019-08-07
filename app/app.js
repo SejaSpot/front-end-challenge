d60443b4001464e2d8fb39b443bdaad4be18756d
Alline Marjorie <allinemarjorie@gmail.com>
2019-08-07 00:03:36 -0300

    Instala ng-sanitizer

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@
 // Declare app level module which depends on views, and core components
 angular.module('myApp', [
   'ngRoute',
+  'ngSanitize',
   'myApp.apiSearcher',
   'myApp.home',
   'myApp.artist',
